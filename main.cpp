e6701e0d380e14693145eb2c11a7031a291d7d53
cloudQuant <yunjinqi@gmail.com>
2024-06-11 19:24:49 +0800

    update structure

---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main(){
+    std::cout << "pyfolio cpp run successfully" << std::endl;
+}
---
+++ tests/test_imgui_plot.cpp
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+class ImguiTest : public ::testing::Test {
+protected:
+    // You can add setup and teardown code here if needed
+};
+
+// Test case for dt_str_flags function
+TEST_F(ImguiTest, TestFirst) {
+    EXPECT_EQ(1,1);
+}
